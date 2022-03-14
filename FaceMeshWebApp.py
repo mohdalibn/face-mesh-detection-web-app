a7afe7872c349def565366228ccc60132317caa9
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-14 13:50:20 +0300

    Added App Title and Sidebar markdown

--- FaceMeshWebApp.py
+++ FaceMeshWebApp.py
@@ -15,3 +15,30 @@ import numpy as np
 import tempfile
 import time
 import cv2
+
+
+# Setting the Title of the Streamlit App
+st.title('Face Mesh Detection Web App')
+
+
+# Adding the SideBar Markdown
+st.markdown(
+    """
+
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{width:350px}
+
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
+        width:350px
+        margin-left: -350px
+        }
+    </style>
+
+    """,
+
+    unsafe_allow_html=True,
+
+)
+
+st.sidebar.title('FaceMesh Sidebar')
+st.sidebar.subheader('parameters')
