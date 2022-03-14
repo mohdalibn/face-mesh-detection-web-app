0afdedde740d5a3cc6613b13585d300ce663f6de
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-14 13:01:39 +0300

    Added the required libraries

---
+++ FaceMeshWebApp.py
@@ -0,0 +1,17 @@
+
+"""
+
+    PROJECT: Face Mess Detection Web
+    MADE BY: Mohd Ali Bin Naser
+    GITHUB : github.com/mohdalibn
+
+"""
+
+# Importing the required libraries for the project
+import streamlit as st
+import mediapipe as mp
+from PIL import Image
+import numpy as np
+import tempfile
+import time
+import cv2
